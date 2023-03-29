17d9fa1b9c12bd23d1bddb34b4d4c8a1146523a4
James Dunn <james@lumanox.com>
2023-03-29 19:25:37 -0400

    Streamlined authentication requests using cookies. urlencoded paths to prevent errors when encountering special characters in file and folder names.

--- files/checksum_all_files.sh
+++ files/checksum_all_files.sh
@@ -11,29 +11,43 @@ DOMAIN="$PROD_HOSTNAME"
 # Import secrets from Environment Variables
 KEY="$PROD_API_KEY"
 SECRET="$PROD_API_SECRET"
+authenticate_and_save_session_id () {
+	# Using session API to authenticate
+	# Using basic authentication to get a cookie
+	# Store it in the cookie jar
+	curl -v -X POST -u "${KEY}:${SECRET}" \
+		 -c "data/${DOMAIN}.session.cookie.jar.txt" \
+		 -D "data/${DOMAIN}.session.headers.txt" \
+			"https://${DOMAIN}/api/2/session/"
+}
+# jq can also be used to urlencode strings,
+# which is lucky because curl can't encode the base url, only the data
+escape_path () {
+	escaped_path=$(printf %s $1 | jq -sRr @uri)
+}
 # Intentionally using /api/2.1/path/info as it is unpaginated
 list_files_in () {
-	local folder=$1
-	files=$(curl -sS -u "${KEY}:${SECRET}" \
-	  "https://${DOMAIN}/api/2.1/path/info${folder}/?children=true&format=json" | \
+	escape_path $1
+	files=$(curl -sS -b "data/${DOMAIN}.session.cookie.jar.txt" \
+	  "https://${DOMAIN}/api/2.1/path/info${escaped_path}/?children=true&format=json" | \
 	  jq -r '.children[] | select(.isfile == true).name')
 }
 #
 list_subdirectories_in () {
-	local folder=$1
-	subdirectories=$(curl -sS -u "${KEY}:${SECRET}" \
-	  "https://${DOMAIN}/api/2.1/path/info${folder}/?children=true&format=json" | \
+	escape_path $1
+	subdirectories=$(curl -sS -b "data/${DOMAIN}.session.cookie.jar.txt" \
+	  "https://${DOMAIN}/api/2.1/path/info${escaped_path}/?children=true&format=json" | \
 	  jq -r '.children[] | select(.isdir == true).name')
 }
 #
 checksum_file () {
-	local le_path=$1
+	escape_path $1
 	# Request Checksum, get task URL
 	local task_url=$(curl -v -X POST \
-						  -u "${KEY}:${SECRET}" \
+						  -b "data/${DOMAIN}.session.cookie.jar.txt" \
 						  "https://${DOMAIN}/api/2/path/oper/checksum/" \
 						  -D "data/${DOMAIN}.checksum.headers.txt" \
-		 				  -d "path=${le_path}" \
+		 				  -d "path=${escaped_path}" \
 						  -d "algorithm=SHA512" | \
 				   jq -r '.url')
 	# echo "Task URL: ${task_url}"
@@ -41,10 +55,11 @@ checksum_file () {
 	#
 	sleep 2
 	# Poll task URL and retry if it's not ready
-	local query=".result.result.checksums.\""${le_path}"\""
+	local query=".result.result.checksums.\""${escaped_path}"\""
 	# echo "Query: ${query}"
 	# read -p "Press enter to continue"
-	local checksum=$(curl -v -u "${KEY}:${SECRET}" \
+	local checksum=$(curl -v \
+						  -b "data/${DOMAIN}.session.cookie.jar.txt" \
 						  "https://${DOMAIN}${task_url}" \
 						  -D "data/${DOMAIN}.task.headers.txt" \
 		    			  --max-time 120 \
@@ -56,27 +71,34 @@ checksum_file () {
 	# echo "Checksum: ${checksum}"
 	# read -p "Press enter to continue"
 	# Write checksum to file attributes
-	curl -v -X POST -u "${KEY}:${SECRET}" \
+	curl -v -X POST \
+		 -b "data/${DOMAIN}.session.cookie.jar.txt" \
 		 -d "attributes=SHA512=${checksum}" \
-		 "https://${DOMAIN}/api/2/path/info${le_path}"
+		 "https://${DOMAIN}/api/2/path/info${escaped_path}"
 }
 #
 checksum_all_files_in () {
-	local folder=$1
+	escape_path $1
 	# get files from directory listing
-	list_files_in "${folder}"
+	list_files_in "${escaped_path}"
 	if [[ -n "$files" ]]; then
 		for file in $files; do
-			# echo "${folder}/${file}"
-			checksum_file "${folder}/${file}"
+			local escaped_path1 = $escaped_path
+			escape_path $file
+			local escaped_file1 = $escaped_path
+			# echo "${escaped_path1}/${escaped_file1}"
+			checksum_file "${escaped_path1}/${escaped_file1}"
 		done
 	fi
-	list_subdirectories_in "${folder}"
+	list_subdirectories_in "${escaped_path}"
 	# loop until there are no more subdirectories to crawl
 	if [[ -n "$subdirectories" ]]; then
 		for subdirectory in $subdirectories; do
-			# echo "${folder}/${subdirectory}"
-			checksum_all_files_in "${folder}/${subdirectory}"
+			local escaped_path1 = $escaped_path
+			escape_path $subdirectory
+			local escaped_path2 = $escaped_path
+			# echo "${escaped_path1}/${escaped_path2}"
+			checksum_all_files_in "${escaped_path1}/${escaped_path2}"
 		done
 	fi
 }
@@ -84,6 +106,5 @@ checksum_all_files_in () {
 checksum_site () {
 	checksum_all_files_in "/"
 }
+authenticate_and_save_session_id
 checksum_site
-# TODO: Optimize login request via cookie jar viz activity_v3_export
-# TODO: URLencode folder and file names to prevent errors e.g. te&st.jpg
