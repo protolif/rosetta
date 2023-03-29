ca3ac4a9d311ddcfeb9c39b5d7cba271124724b7
James Dunn <james@lumanox.com>
2023-03-29 13:59:09 -0400

    Refactored using functions, made it recursive, and added checks for null values

--- files/checksum_all_files.sh
+++ files/checksum_all_files.sh
@@ -11,48 +11,81 @@ DOMAIN="$PROD_HOSTNAME"
 # Import secrets from Environment Variables
 KEY="$PROD_API_KEY"
 SECRET="$PROD_API_SECRET"
-FOLDER="/test_files"
-# get files from directory listing
-FILENAMES=$(curl -sS -u "${KEY}:${SECRET}" \
-  "https://${DOMAIN}/api/2.1/path/info${FOLDER}/?children=true&format=json" | \
-  jq -r '.children[] | select(.isfile == true).name')
-# echo "${FILENAMES}"
-for FILE in $FILENAMES; do
-	#
-	LE_PATH="${FOLDER}/${FILE}"
+# Intentionally using /api/2.1/path/info as it is unpaginated
+list_files_in () {
+	local folder=$1
+	files=$(curl -sS -u "${KEY}:${SECRET}" \
+	  "https://${DOMAIN}/api/2.1/path/info${folder}/?children=true&format=json" | \
+	  jq -r '.children[] | select(.isfile == true).name')
+}
+#
+list_subdirectories_in () {
+	local folder=$1
+	subdirectories=$(curl -sS -u "${KEY}:${SECRET}" \
+	  "https://${DOMAIN}/api/2.1/path/info${folder}/?children=true&format=json" | \
+	  jq -r '.children[] | select(.isdir == true).name')
+}
+#
+checksum_file () {
+	local le_path=$1
 	# Request Checksum, get task URL
-	TASK_URL=$(curl -v -X POST \
-					-u "${KEY}:${SECRET}" \
-					"https://${DOMAIN}/api/2/path/oper/checksum/" \
-					-D "data/${DOMAIN}.checksum.headers.txt" \
-	 				-d "path=${LE_PATH}" \
-					-d "algorithm=SHA512" | \
-			 jq -r '.url')
-	# echo "Task URL: ${TASK_URL}"
+	local task_url=$(curl -v -X POST \
+						  -u "${KEY}:${SECRET}" \
+						  "https://${DOMAIN}/api/2/path/oper/checksum/" \
+						  -D "data/${DOMAIN}.checksum.headers.txt" \
+		 				  -d "path=${le_path}" \
+						  -d "algorithm=SHA512" | \
+				   jq -r '.url')
+	# echo "Task URL: ${task_url}"
 	# read -p "Press enter to continue"
 	#
 	sleep 2
 	# Poll task URL and retry if it's not ready
-	QUERY=".result.result.checksums.\""${LE_PATH}"\""
-	# echo "Query: ${QUERY}"
+	local query=".result.result.checksums.\""${le_path}"\""
+	# echo "Query: ${query}"
 	# read -p "Press enter to continue"
-	CHECKSUM=$(curl -v -u "${KEY}:${SECRET}" \
-					"https://${DOMAIN}${TASK_URL}" \
-					-D "data/${DOMAIN}.task.headers.txt" \
-	    			--max-time 120 \
-	    			--retry 60 \
-	    			--retry-delay 2 \
-	    			--retry-max-time 120 \
-	    			--retry-all-errors | \
-			 jq -r $QUERY)
-	# echo "Checksum: ${CHECKSUM}"
+	local checksum=$(curl -v -u "${KEY}:${SECRET}" \
+						  "https://${DOMAIN}${task_url}" \
+						  -D "data/${DOMAIN}.task.headers.txt" \
+		    			  --max-time 120 \
+		    			  --retry 60 \
+		    			  --retry-delay 2 \
+		    			  --retry-max-time 120 \
+		    			  --retry-all-errors | \
+				   jq -r $query)
+	# echo "Checksum: ${checksum}"
 	# read -p "Press enter to continue"
 	# Write checksum to file attributes
 	curl -v -X POST -u "${KEY}:${SECRET}" \
-		 -d "attributes=SHA512=${CHECKSUM}" \
-		 "https://${DOMAIN}/api/2/path/info${LE_PATH}"
-done
-# TODO: Refactor this into a function
-# TODO: Implement recursion
-# To get only subfolders
-# query simliar to FILENAMES but where .isdir == true
+		 -d "attributes=SHA512=${checksum}" \
+		 "https://${DOMAIN}/api/2/path/info${le_path}"
+}
+#
+checksum_all_files_in () {
+	local folder=$1
+	# get files from directory listing
+	list_files_in "${folder}"
+	if [[ -n "$files" ]]
+	then
+		for file in $files; do
+			# echo "${folder}/${file}"
+			checksum_file "${folder}/${file}"
+		done
+	fi
+	list_subdirectories_in "${folder}"
+	# loop until there are no more subdirectories to crawl
+	if [[ -n "$subdirectories" ]]
+	then
+		for subdirectory in $subdirectories; do
+			# echo "${folder}/${subdirectory}"
+			checksum_all_files_in "${folder}/${subdirectory}"
+		done
+	fi
+}
+#
+checksum_site () {
+	checksum_all_files_in "/"
+}
+checksum_site
+# TODO: Optimize login request via cookie jar viz activity_v3_export
+# TODO: URLencode folder and file names to prevent errors e.g. te&st.jpg
