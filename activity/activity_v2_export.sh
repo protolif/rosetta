cc2f55270fa46831305f718103dcf481e906c171
James Dunn <james@lumanox.com>
2023-04-14 17:21:15 -0400

    fixing whitespace in this file with soft tabs

--- activity/activity_v2_export.sh
+++ activity/activity_v2_export.sh
@@ -27,13 +27,13 @@ FILE_PREFIX="data/${DOMAIN}.activity_from_${START_ON}_to_${END_ON}"
 # Call the v2 API via curl
 # curl documentation: https://curl.se/docs/manpage.html
 curl -v -u "${KEY}:${SECRET}" \
-    --max-time 120 \
-    --retry 10 \
-    --retry-delay 90 \
-    --retry-max-time 120 \
-	--retry-all-errors \
-	"https://${DOMAIN}/api/2/activity/?timestamp_min=${START_ON}&timestamp_max=${END_ON}&limit=${LIMIT}&page=[1-${PAGES}]&format=csv" \
-	--output "${FILE_PREFIX}_page_#1.csv"
+  --max-time 120 \
+  --retry 10 \
+  --retry-delay 90 \
+  --retry-max-time 120 \
+  --retry-all-errors \
+  "https://${DOMAIN}/api/2/activity/?timestamp_min=${START_ON}&timestamp_max=${END_ON}&limit=${LIMIT}&page=[1-${PAGES}]&format=csv" \
+  --output "${FILE_PREFIX}_page_#1.csv"
 
 # Combine all pages into a single file
 cat $FILE_PREFIX* >> "${FILE_PREFIX}_combined.csv"
